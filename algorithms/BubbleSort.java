0f686d5f867f2c5914f9fc34662892d7a5cbfe28
Shourya Sharma <shouryasharma@gmail.com>
2022-09-28 07:04:04 +0530

    correction to bubble sort algorithm

--- algorithms/BubbleSort.java
+++ algorithms/BubbleSort.java
@@ -1,24 +1,27 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	// Bubble Sort
-	protected int[] bubbleSort(int[] array) {
+	public int[] bubbleSort(int[] array) {
 		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array.length; j++) {
+			for (int j = 1; j < (array.length - i); j++) {
 				int tempForSwap = 0;
-				if (array[i] < array[j]) {
-					tempForSwap = array[i];
-					array[i] = array[j];
+				if (array[j - 1] > array[j]) {
+					tempForSwap = array[j - 1];
+					array[j - 1] = array[j];
 					array[j] = tempForSwap;
 				}
 			}
 		}
-
 		return array;
 	}
 
-	public static void name(String[] args) {
-		System.out.println("Hello");
+	public static void main(String[] args) {
+		int[] array = { 8, 6, 5, 3, 6, 8, 9 };
+		BubbleSort bs = new BubbleSort();
+		System.out.println(Arrays.toString(bs.bubbleSort(array)));
 	}
 }
