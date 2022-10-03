5242c3bf71949fdf754262682ad5f46bc9254725
Shourya Sharma <shouryasharma@gmail.com>
2022-10-04 04:59:54 +0530

    updated .gitignore & added another case in StackMultiParenthesesMatch

--- problems/StackMultiParanthesesMatch.java
+++ problems/StackMultiParanthesesMatch.java
@@ -3,39 +3,40 @@ package problems;
 import java.util.Stack;
 
 public class StackMultiParanthesesMatch {
-    public static void main(String[] args) {
-        try {
-            System.out.println(match("{(())}"));
-            System.out.println(match("{(()}"));
-            System.out.println(match("@@"));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+	public static void main(String[] args) {
+  	try {
+   		System.out.println(match("(){({}())}"));
+      System.out.println(match("{(())}"));
+      System.out.println(match("{(()}"));
+      System.out.println(match("@@"));
+    } catch (Exception e) {
+      	System.out.println(e.getMessage());
     }
+  }
 
-    public static boolean match(String parens) throws Exception {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < parens.length(); i++) {
-            char currentChar = parens.charAt(i);
-            if (currentChar == '{' || currentChar == '(') {
-                stack.push(currentChar);
-            } else if (currentChar == '}' || currentChar == ')') {
-                if (!stack.empty()) {
-                    char topChar = stack.peek();
-                    if ((topChar == '(' && currentChar == ')') || (topChar == '{' && currentChar == '}')) {
-                        stack.pop();
-                    }
-                } else {
-                    return false;
-                }
-            } else {
-                throw new Exception("Invalid Character");
-            }
-        }
-        if (stack.empty()) {
-            return true;
+  public static boolean match(String parens) throws Exception {
+  	Stack<Character> stack = new Stack<>();
+      for (int i = 0; i < parens.length(); i++) {
+      	char currentChar = parens.charAt(i);
+        if (currentChar == '{' || currentChar == '(') {
+        	stack.push(currentChar);
+        } else if (currentChar == '}' || currentChar == ')') {
+       		if (!stack.empty()) {
+        		char topChar = stack.peek();
+          if ((topChar == '(' && currentChar == ')') || (topChar == '{' && currentChar == '}')) {
+          	stack.pop();
+          }
+       	} else {
+        	return false;
+       	}
         } else {
-            return false;
+        	throw new Exception("Invalid Character");
         }
-    }
+     	}
+      if (stack.empty()) {
+      	return true;
+     	} else {
+      	return false;
+      }
+  }
 }
