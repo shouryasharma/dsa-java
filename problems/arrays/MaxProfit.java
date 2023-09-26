7ff9a5023d8ae57235804843d6fc8f0287dfd5dd
shouryasharma <shouryasharma@gmail.com>
2023-09-26 05:38:28 +0530

    max profit

--- problems/arrays/MaxProfit.java
+++ problems/arrays/MaxProfit.java
@@ -1,57 +1,47 @@
 package arrays;
+
+// leetcode 121. Best Time to Buy and Sell Stock
 public class MaxProfit {
     static int maxProfit(int[] prices) {
-        int minPriceIndex = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < prices[minPriceIndex])
-                minPriceIndex = i;
-        }
-
-        int maxPriceIndex = 0;
-        if (minPriceIndex < prices.length - 1)
-            maxPriceIndex = minPriceIndex;
-
-        for (int i = maxPriceIndex; i < prices.length; i++) {
-            if (prices[i] > prices[maxPriceIndex])
-                maxPriceIndex = i;
-        }
-
-        if (maxPriceIndex > minPriceIndex)
-            return prices[maxPriceIndex] - prices[minPriceIndex];
-        else {
+        if (prices.length == 0)
             return 0;
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 0; i < prices.length; i++) {
+            int price = prices[i];
+            if (price < minPrice)
+                minPrice = price;
+            int profit = price - minPrice;
+            if (profit > maxProfit)
+                maxProfit = profit;
         }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
+        int[] prices = {};
+        int profit = maxProfit(prices);
+        System.out.println("Test case 1: Maximum profit: " + profit); // 0
+
         int[] prices1 = { 7, 1, 5, 3, 6, 4 };
         int profit1 = maxProfit(prices1);
-        System.out.println("Test case 1: Maximum profit: " + profit1); // prints "Maximum profit: 5"
+        System.out.println("Test case 1: Maximum profit: " + profit1); // 5
 
         int[] prices2 = { 7, 6, 5, 4, 3, 2, 1 };
         int profit2 = maxProfit(prices2);
-        System.out.println("Test case 2: Maximum profit: " + profit2); // prints "Maximum profit: 0"
+        System.out.println("Test case 2: Maximum profit: " + profit2); // 0
 
         int[] prices3 = { 1, 2, 3, 4, 5, 6, 7 };
         int profit3 = maxProfit(prices3);
-        System.out.println("Test case 3: Maximum profit: " + profit3); // prints "Maximum profit: 6"
+        System.out.println("Test case 3: Maximum profit: " + profit3); // 6
 
         int[] prices4 = { 3, 2, 6, 5, 0, 3 };
         int profit4 = maxProfit(prices4);
-        System.out.println("Test case 4: Maximum profit: " + profit4); // prints "Maximum profit: 4"
+        System.out.println("Test case 4: Maximum profit: " + profit4); // 4
 
         int[] prices5 = { 2, 4, 1 };
         int profit5 = maxProfit(prices5);
-        System.out.println("Test case 5: Maximum profit: " + profit5); // prints "Maximum profit: 2"
-
-        /*
-         * EXPECTED OUTPUT:
-         * ----------------
-         * Test case 1: Maximum profit: 5
-         * Test case 2: Maximum profit: 0
-         * Test case 3: Maximum profit: 6
-         * Test case 4: Maximum profit: 4
-         * Test case 5: Maximum profit: 2
-         */
+        System.out.println("Test case 5: Maximum profit: " + profit5); // 2
     }
 }
