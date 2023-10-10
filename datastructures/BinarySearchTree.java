819392670e06cbb8281555b87dc09e987a2fa8d6
shouryasharma <shouryasharma@gmail.com>
2023-10-10 21:12:51 +0530

    DFS preorder

--- datastructures/BinarySearchTree.java
+++ datastructures/BinarySearchTree.java
@@ -13,6 +13,7 @@ public class BinarySearchTree {
         }
     }
 
+    // iterative insert
     public boolean insert(int value) {
         Node newNode = new Node(value);
 
@@ -43,19 +44,62 @@ public class BinarySearchTree {
         }
     }
 
+    // recursive insert
+    private Node rInsert(Node currentNode, int value) {
+        if (currentNode == null)
+            return new Node(value);
+        if (value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        } else if (value > currentNode.value) { // try to replace with else
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    public void rInsert(int value) {
+        if (root == null)
+            root = new Node(value);
+        rInsert(root, value);
+    }
+
+    // iterative contains
     public boolean contains(int value) {
         Node temp = root;
-
         while (temp != null) {
-            if (value < temp.value) {
+            if (value < temp.value)
                 temp = temp.left;
-            } else if (value > temp.value) {
+            else if (value > temp.value)
                 temp = temp.right;
-            } else {
+            else
                 return true;
-            }
-        }
 
+        }
         return false;
+    }
+
+    // recursive contains
+    private boolean rContains(Node currentNode, int value) {
+        if (currentNode == null)
+            return false;
+        if (value == currentNode.value)
+            return true;
+        if (value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        } else {
+            return rContains(currentNode.left, value);
+        }
+    }
+
+    public boolean rContains(int value) {
+        return rContains(root, value);
+    }
+
+    // dfsPreOrder Traversal
+    public void dfsPreOrder(Node node) {
+        if (node != null) {
+            System.out.println(node.value);
+            dfsPreOrder(node.left);
+            dfsPreOrder(node.right);
+        }
     }
 }
--- datastructures/Main.java
+++ datastructures/Main.java
@@ -3,14 +3,20 @@ package datastructures;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
-        bst.insert(11);
-        bst.insert(15);
-        bst.insert(9);
-        bst.insert(10);
-        bst.insert(7);
+
+        bst.rInsert(11);
+        bst.rInsert(15);
+        bst.rInsert(9);
+        bst.rInsert(10);
+        bst.rInsert(7);
 
         System.out.println("Root: " + bst.root.value);
 
         System.out.println("Root: " + bst.contains(5));
+        System.out.println("Root: " + bst.rContains(7));
+        System.out.println("Root: " + bst.rContains(5));
+
+        System.out.println("dfsPreOrder:");
+        bst.dfsPreOrder(bst.root);
     }
 }
